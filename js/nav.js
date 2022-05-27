5acee19571b5efd0303ad6309a79f97ba3c7896f
Ishaan Singh <sibrij6@gmail.com>
2022-05-27 20:03:18 +0530

    updated files

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
